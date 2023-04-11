95af2b3821d37d126159c6bfb803bee7310222e5
JoseCarrillo <josecarrival1@gmail.com>
2023-04-11 13:52:37 -0700

    version6

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
-import Gallery from './pexels'
+import Gallery from './pexels';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { useRef } from "react";
+
 
 function App() {
 
-  
+
+  const contentInicioRef = useRef(null);
+
+  const handleClick = () => {
+    contentInicioRef.current.scrollIntoView({ behavior: "smooth" });
+  };
 
   return (
     <div className="App">
@@ -31,11 +39,17 @@ function App() {
                 <div class="relative mt-2 rounded-md shadow-sm">
                   <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
                   </div>
-                  <input type="text" name="price" id="price" class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Categoria" />
 
                 </div>
-              </div>        <div class="mt-10 flex items-center justify-center gap-x-6">
-                <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
+              </div>   
+
+
+
+              <div class="mt-10 flex items-center justify-center gap-x-6">
+              <a   href="#contentInicio"
+              onClick={handleClick} class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Conocenos</a>
+          
+
               </div>
             </div>
           </div>
@@ -52,8 +66,8 @@ function App() {
           </div>
         </div>
       </div>
-
-      <h1 className='tittle'>Worldly Views</h1>  
+    
+      <h1 ref={contentInicioRef} id="contentInicio" className="tittle">Worldly Views</h1>  
 
       <div > 
       <div className='contentpresent'>
